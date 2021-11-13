d1a01b03d48d026ade50068e2e23ec6bddf8e0b1
Vojtěch Váchal <vachalvo@students.zcu.cz>
2021-11-13 09:19:46 +0100

    BE Remove Jwt - preparation to start again

--- backend/src/main/java/com/kiv/pia/backend/controller/UserController.java
+++ backend/src/main/java/com/kiv/pia/backend/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.kiv.pia.backend.controller;
 
-import com.kiv.pia.backend.config.JwtTokenProvider;
 import com.kiv.pia.backend.model.*;
 import com.kiv.pia.backend.model.request.AuthenticateBody;
 import com.kiv.pia.backend.model.request.RegistrationBody;
@@ -16,6 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -34,67 +34,23 @@ public class UserController {
     private static Logger log = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
-    private AuthenticationManager authenticationManager;
-
-    @Autowired
-    private JwtTokenProvider tokenProvider;
-
-    @Autowired
-    private RoleRepository roleRepository;
-
-    @Autowired
     private IService<User, UUID> userService;
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private RoleRepository roleRepository;
 
     @PostMapping(value = "/register", produces = MediaType.APPLICATION_JSON_VALUE)
     @Transactional
     public ResponseEntity<String> register(@Valid @RequestBody RegistrationBody body) {
-        User user = new User(body.getEmail(), body.getPassword(), body.getFirstName(), body.getLastName());
-        log.info("UserResourceImpl : register");
-        JSONObject jsonObject = new JSONObject();
-        try {
-            user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
-            user.setRole(roleRepository.findByName(RoleType.ROLE_USER.getName()));
 
-            User savedUser = userRepository.save(user);
-            jsonObject.put("message", savedUser.getEmail() + " saved succesfully");
-            return new ResponseEntity<>(jsonObject.toString(), HttpStatus.OK);
-        } catch (JSONException e) {
-            try {
-                jsonObject.put("exception", e.getMessage());
-            } catch (JSONException e1) {
-                e1.printStackTrace();
-            }
-            return new ResponseEntity<>(jsonObject.toString(), HttpStatus.UNAUTHORIZED);
-        }
     }
 
     @PostMapping(value = "/authenticate", produces = MediaType.APPLICATION_JSON_VALUE)
     @Transactional
     public ResponseEntity<String> authenticate(@Valid @RequestBody AuthenticateBody body) {
-        log.info("UserResourceImpl : authenticate");
-        JSONObject jsonObject = new JSONObject();
-        try {
-            Authentication authentication = authenticationManager
-                    .authenticate(new UsernamePasswordAuthenticationToken(body.getEmail(), body.getPassword()));
-            if (authentication.isAuthenticated()) {
-                String email = body.getEmail();
-                jsonObject.put("name", authentication.getName());
-                jsonObject.put("authorities", authentication.getAuthorities());
-                jsonObject.put("token", tokenProvider.createToken(email, new Role(RoleType.ROLE_USER.getName())));
-                return new ResponseEntity<>(jsonObject.toString(), HttpStatus.OK);
-            }
-        } catch (JSONException e) {
-            try {
-                jsonObject.put("exception", e.getMessage());
-            } catch (JSONException e1) {
-                e1.printStackTrace();
-            }
-            return new ResponseEntity<>(jsonObject.toString(), HttpStatus.UNAUTHORIZED);
-        }
-        return null;
+
     }
 
     @GetMapping("/{id}")
