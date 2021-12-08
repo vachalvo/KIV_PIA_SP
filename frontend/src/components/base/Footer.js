309ad9155440635e2e822b3cd8530b87adc99858
Vojtěch Váchal <vachalvo@students.zcu.cz>
2021-12-08 23:02:06 +0100

    FE New footer

--- frontend/src/components/base/Footer.js
+++ frontend/src/components/base/Footer.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import {Navbar, Container} from "react-bootstrap";
 import {useState, useEffect} from "react";
 import {Col} from "react-bootstrap";
+import {Typography} from "@mui/material";
 
 function Footer() {
     const [fullYear, setFullYear] = useState();
@@ -12,12 +13,12 @@ function Footer() {
 
     return (
         <div style={{"padding": "40px"}} >
-            <Navbar fixed="bottom" bg="light" variant="light">
+            <Navbar fixed="bottom" style={{ background: '#1976D2' }}>
                 <Container>
-                    <Col lg={12} className="text-center text-muted">
-                        <div>
+                    <Col className="text-center text-muted">
+                        <Typography color='#FFFFFF'>
                             {fullYear}-{fullYear + 1}, All Rights Reserved by Vojtěch Váchal
-                        </div>
+                        </Typography>
                     </Col>
                 </Container>
             </Navbar>
