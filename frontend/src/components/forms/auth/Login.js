73f5f216ad44d3b1711dfb6f1236ae4a6d3aefac
Vojtěch Váchal <vachalvo@students.zcu.cz>
2021-12-08 23:02:26 +0100

    FE New login page implemented

--- frontend/src/components/forms/auth/Login.js
+++ frontend/src/components/forms/auth/Login.js
@@ -1,146 +1,278 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import {useState} from "react";
-import {useHistory} from "react-router-dom";
 
-import {Form, Button, Card, InputGroup, Row, Col, Alert, Container} from "react-bootstrap";
-
-import Validation from "../../../global/validations";
 import AuthService from "../../../services/auth-service";
+import FormService from "../../../services/form-service";
+import {
+    Avatar,
+    Card,
+    Grid,
+    Link,
+    Typography,
+    Stack,
+    CardContent,
+    Container,
+    InputAdornment,
+    InputLabel,
+    FormControl,
+    OutlinedInput,
+    IconButton,
+    FormHelperText,
+    Divider,
+    AlertTitle, Alert, Collapse
+} from "@mui/material";
+import {
+    AlternateEmailOutlined, Close,
+    LockOutlined,
+    LoginOutlined,
+    Visibility,
+    VisibilityOff
+} from "@mui/icons-material";
+import {Col, Row} from "react-bootstrap";
 
-const initData = {
-    email: '',
-    password: ''
-};
-
-const initFeedback = {
-    email: '',
-    password: ''
-}
+import '../../../styles/components/login.css';
+import LoadingButton from "@mui/lab/LoadingButton";
 
 function Login(props) {
-    const [data, setData] = useState(initData);
-    const [loading, setLoading] = useState(false);
-    const [feedback, setFeedback] = useState(initFeedback);
+    const [values, setValues] = useState({
+        email: '',
+        password: '',
+        emailFeedback: '',
+        passwordFeedback: '',
+        showPassword: false,
+        loading: false,
+        openAlert: false,
+        alertText: ''
+    });
 
-    const _validateForm = () => {
-        const { email, password } = data;
-        const newFeedback = {
-            ...initFeedback
-        };
-
-        newFeedback.email = Validation.validateEmail(email);
-        newFeedback.password = Validation.validateLoginPassword(password);
-
-        return newFeedback;
+    const onChange = (prop) => (event) => {
+        setValues({ ...values,
+            [prop]: event.target.value,
+            emailFeedback: prop === 'email' ? '' : values.emailFeedback,
+            passwordFeedback: prop === 'password' ? '' : values.passwordFeedback,
+        });
+    }
+    const handleClickShowPassword = () => {
+        setValues({
+            ...values,
+            showPassword: !values.showPassword,
+        });
     };
 
-    const _onChange = (event) => {
-        const { name, value } = event.target;
-        setData({ ...data, [name]: value });
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
 
-        // Check and see if errors exist, and remove them from the error object:
-        if (!!feedback[name]){
-            setFeedback({
-                ...feedback,
-                [name]: ''
+    const onBlurEmail = () => {
+        FormService.validateLoginEmail(values.email).then((response) => {
+            setValues({
+                ...values,
+                emailFeedback: ''
             });
+        }).catch((err) => {
+            const data = err.response.data;
+            if(data.errorMsg) {
+                setValues({
+                    ...values,
+                    emailFeedback: data.errorMsg
+                });
+            }
+        });
+    };
+    const onBlurPassword = () => {
+        FormService.validatePassword(values.password).then((response) => {
+            setValues({
+                ...values,
+                passwordFeedback: ''
+            });
+        }).catch((err) => {
+            const data = err.response.data;
+            if(data.errorMsg) {
+                setValues({
+                    ...values,
+                    passwordFeedback: data.errorMsg
+                });
+            }
+        });
+    };
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            _login(event);
         }
     };
 
     const _login = (event) => {
         event.preventDefault();
 
-        setLoading(true);
-        const newFeedback = _validateForm()
+        setValues({
+            ...values,
+            loading: true,
+        });
 
-        if (!Object.values(newFeedback).every(x => x === '')) {
-            setFeedback(newFeedback);
-            setLoading(false);
-            return;
-        }
-
+        const data = {
+            'email': values.email,
+            'password': values.password
+        };
         AuthService.login(data).then(
             () => {
                 props.history.push("/");
                 window.location.reload();
-            },
-            (error) => {
-                alert(error);
-
-                setLoading(false);
             }
-        );
+        ).catch((err) => {
+            const data = err.response.data;
+            const newFeedback = {
+                emailFeedback: values.emailFeedback,
+                passwordFeedback: values.passwordFeedback
+            };
+            let errorMsg = 'Unknown error';
 
-       /* fetch(getUri("/users/authenticate"),initFetch("POST", data))
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                if(data.fieldErrors) {
-                    const newFeedback = {
-                        ...initFeedback
-                    };
-                    data.fieldErrors.forEach(fieldError => {
-                        newFeedback[fieldError.field] = fieldError.message;
-                    });
-                    setFeedback(newFeedback);
-                } else if(data.error){
+            if(data.fieldErrors){
+                errorMsg = 'Some fields are not filled properly'
 
-                }
-                else {
-                    history.push('/feed');
-                }
-            })
-            .catch((err) => console.log(err));*/
+
+                data.fieldErrors.forEach(fieldError => {
+                    newFeedback[fieldError.field + 'Feedback'] = fieldError.message;
+                });
+            }
+            setValues({
+                ...values,
+                loading: false,
+                openAlert: true,
+                alertText: err.response.data.error ? err.response.data.error : errorMsg,
+                ...newFeedback
+            });
+        });
+
+        setValues({
+            ...values,
+            loading: false,
+        });
     };
 
-    return (
-        <>
-            <Card style={{margin: "50px", padding: "20px"}}>
-                <h2 className={"text-center"}>Login</h2>
-                <Form>
-                    <Row className="mb-3">
-                        <Form.Group md="12">
-                            <Form.Label>Email</Form.Label>
-                            <InputGroup>
-                                <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-                                <Form.Control
-                                    type="text"
-                                    name="email"
-                                    placeholder="Username"
-                                    onChange={_onChange}
-                                    value={data.email}
-                                    required
-                                    isInvalid={ !!feedback.email }
-                                />
-                                <Form.Control.Feedback type="invalid">{feedback.email}</Form.Control.Feedback>
-                            </InputGroup>
-                        </Form.Group>
-                    </Row>
-                    <Row className="mb-3">
-                        <Form.Group md="12">
-                            <Form.Label>Password</Form.Label>
-                            <InputGroup>
-                                <Form.Control
-                                    type="password"
-                                    name="password"
-                                    placeholder="Password"
-                                    onChange={_onChange}
-                                    value={data.password}
-                                    required
-                                    isInvalid={ !!feedback.password }
-                                />
-                                <Form.Control.Feedback type="invalid">{feedback.password}</Form.Control.Feedback>
-                            </InputGroup>
-                        </Form.Group>
-                    </Row>
-                    <div style={{"textAlign": "center"}}>
-                        <Button type="submit" onClick={_login} size="lg" disabled={loading} variant="outline-success">
-                            {loading ? 'Loading…' : 'Login'}
-                        </Button>
-                    </div>
-                </Form>
+    const renderAlert = () => {
+        return (
+            <Collapse in={values.openAlert}>
+                <Alert
+                    severity="error"
+                    action={
+                    <IconButton
+                        aria-label="close"
+                        color="inherit"
+                        size="small"
+                        onClick={() => {
+                            setValues({
+                                ...values,
+                                openAlert: false
+                            });
+                        }}
+                    >
+                        <Close fontSize="inherit" />
+                    </IconButton>
+                }
+                       sx={{ mb: 2 }}>
+                    <AlertTitle><strong>{values.alertText}</strong></AlertTitle>
+                </Alert>
+            </Collapse>
+        );
+    }
+    const renderForm = () => {
+        return (
+            <Card style={{margin: "20px 0 0 0"}}>
+                <CardContent className="text-center">
+                    <Grid align='center'>
+                        <Avatar>
+                            <LockOutlined/>
+                        </Avatar>
+                        <Typography sx={{mb: 2}} variant={'h4'}>
+                            Login
+                        </Typography>
+                    </Grid>
+                    <Stack spacing={2}>
+                        <FormControl
+                            variant="outlined"
+                            error={values.emailFeedback !== ''}
+                            fullWidth
+                        >
+                            <InputLabel htmlFor="outlined-adornment-email">Email</InputLabel>
+                            <OutlinedInput
+                                id="outlined-adornment-email"
+                                type='email'
+                                onKeyDown={handleKeyDown}
+                                value={values.email}
+                                placeholder='Enter email'
+                                onBlur={onBlurEmail}
+                                onChange={onChange('email')}
+                                startAdornment={<InputAdornment position="start"><AlternateEmailOutlined /></InputAdornment>}
+                                label="Email"
+                            />
+                            <FormHelperText id="email-error-text">{values.emailFeedback}</FormHelperText>
+                        </FormControl>
+                        <FormControl
+                            sx={{ m: 1 }}
+                            variant="outlined"
+                            error={values.passwordFeedback !== ''}
+                            fullWidth
+                        >
+                            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                            <OutlinedInput
+                                id="outlined-adornment-password"
+                                type={values.showPassword ? 'text' : 'password'}
+                                value={values.password}
+                                placeholder='Enter password'
+                                onBlur={onBlurPassword}
+                                onKeyDown={handleKeyDown}
+                                onChange={onChange('password')}
+                                startAdornment={<InputAdornment position="start"><LockOutlined /></InputAdornment>}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={handleClickShowPassword}
+                                            onMouseDown={handleMouseDownPassword}
+                                            edge="end"
+                                        >
+                                            {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                                label="Password"
+                            />
+                            <FormHelperText id="password-error-text">{values.passwordFeedback}</FormHelperText>
+                        </FormControl>
+                        <Divider />
+                        <LoadingButton
+                            variant="contained"
+                            onClick={_login}
+                            loading={values.loading}
+                            fullWidth
+                            loadingPosition="start"
+                            startIcon={<LoginOutlined />}
+                        >
+                            <span>LOGIN</span>
+                        </LoadingButton>
+                        <Typography > New to Squirrel?
+                            <Link onClick={() => props.history.push("/signup")} sx={{ml: 1}}>
+                                Create an account
+                            </Link>
+                            .
+                        </Typography>
+                    </Stack>
+                </CardContent>
             </Card>
-        </>
+        );
+    }
+    return (
+        <div style={{margin: '20px 0 20px 0'}}>
+            <Container>
+                <Row className="justify-content-md-center">
+                    <Col md={8} lg={6}>
+                        {renderAlert()}
+                        {renderForm()}
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     );
 }
 
---
+++ frontend/src/services/form-service.js
@@ -0,0 +1,97 @@
+import axios from "axios";
+import authHeader from "./auth-header";
+import Constants from "../global/constants";
+
+const API_URL = Constants.BASE_URL + '/forms';
+
+const validateRegistrationEmail = (email) => {
+    const url = API_URL + '/registration/email';
+    return axios.get(url, {
+        params: {
+            email: email
+        },
+        headers: authHeader()
+    });
+};
+
+const validateLoginEmail = (email) => {
+    const url = API_URL + '/login/email';
+    return axios.get(url, {
+        params: {
+            email: email
+        },
+        headers: authHeader()
+    });
+};
+
+const validateFirstName = (firstName) => {
+    const url = API_URL + '/firstName';
+    return axios.get(url, {
+        params: {
+            firstName: firstName
+        },
+        headers: authHeader()
+    });
+};
+
+const validateLastName = (lastName) => {
+    const url = API_URL + '/lastName';
+    return axios.get(url, {
+        params: {
+            lastName: lastName
+        },
+        headers: authHeader()
+    });
+};
+
+const validateGender = (gender) => {
+    const url = API_URL + '/gender';
+    return axios.get(url, {
+        params: {
+            gender: gender
+        },
+        headers: authHeader()
+    });
+};
+
+const validatePassword = (password) => {
+    const url = API_URL + '/password';
+    return axios.get(url, {
+        params: {
+            password: password
+        },
+        headers: authHeader()
+    });
+};
+
+const validateReEnterPassword = (reEnterPassword) => {
+    const url = API_URL + '/reEnterPassword';
+    return axios.get(url, {
+        params: {
+            reEnterPassword: reEnterPassword
+        },
+        headers: authHeader()
+    });
+};
+
+const validateRegistrationPasswords = (password, reEnterPassword) => {
+    const url = API_URL + '/passwords';
+    return axios.get(url, {
+        params: {
+            password: password,
+            reEnterPassword: reEnterPassword
+        },
+        headers: authHeader()
+    });
+};
+
+export default {
+    validateLoginEmail,
+    validateRegistrationEmail,
+    validateGender,
+    validatePassword,
+    validateLastName,
+    validateReEnterPassword,
+    validateFirstName,
+    validateRegistrationPasswords
+};
