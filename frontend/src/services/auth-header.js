8080a9d55a5a1df3273838ae70c292a6d27960fd
Vojtěch Váchal <vachalvo@students.zcu.cz>
2021-12-16 17:08:07 +0100

    CSRF protection implementation begin

--- frontend/src/services/auth-header.js
+++ frontend/src/services/auth-header.js
@@ -1,14 +1,24 @@
 import Constants from "../global/constants";
 
-export default function authHeader() {
+export default function authHeader(csfrToken = undefined) {
     const token = JSON.parse(sessionStorage.getItem(Constants.SESSION_STORAGE_TOKEN));
-    const headers = {};
+    const headers = Constants.HEADERS;
 
     if (token) {
+        if(csfrToken){
+            return {
+                ...headers,
+                Authorization: Constants.AUTH_TYPE + ' ' + token,
+                'X-Authorization': Constants.AUTH_TYPE + ' ' + token,
+                'X-XSRF-TOKEN': csfrToken,
+                'X-CSRF-TOKEN': csfrToken,
+            };
+        }
         return {
             ...headers,
             Authorization: Constants.AUTH_TYPE + ' ' + token,
             'X-Authorization': Constants.AUTH_TYPE + ' ' + token,
+
         };
     }
 
