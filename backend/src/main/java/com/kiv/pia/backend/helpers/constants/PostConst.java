04bd5f219629a82c3c5ac31ad39f73142e635261
Vojtěch Váchal <vachalvo@students.zcu.cz>
2021-12-16 17:29:29 +0100

    BE add max size to post content

--- backend/src/main/java/com/kiv/pia/backend/helpers/constants/PostConst.java
+++ backend/src/main/java/com/kiv/pia/backend/helpers/constants/PostConst.java
@@ -3,9 +3,13 @@ package com.kiv.pia.backend.helpers.constants;
 public class PostConst {
     private PostConst() { }  // Prevents instantiation
 
+    public static final int HEADER_MAX_SIZE = 100;
+    public static final int CONTENT_MAX_SIZE = 1000;
+
     public static final String HEADER_IS_BLANK = "Header is a required field!";
     public static final String CONTENT_IS_BLANK  = "Content is a required field!";
 
-    public static final String HEADER_SIZE = "Header must have lenght between 3 - 100!";
-    public static final String CONTENT_SIZE  = "Content must have lenght at least 3!";
+    public static final String HEADER_SIZE = "Header must have lenght between 3 - " + PostConst.HEADER_MAX_SIZE + "!";
+    public static final String CONTENT_SIZE = "Content must have lenght between 3 - " + PostConst.CONTENT_MAX_SIZE + "!";
+
 }
--- backend/src/main/java/com/kiv/pia/backend/model/request/PostCreateBody.java
+++ backend/src/main/java/com/kiv/pia/backend/model/request/PostCreateBody.java
@@ -9,11 +9,11 @@ import javax.validation.constraints.Size;
 @Data
 public class PostCreateBody {
     @NotBlank(message = PostConst.HEADER_IS_BLANK)
-    @Size(min = 3, max = 100, message = PostConst.HEADER_SIZE)
+    @Size(min = 3, max = PostConst.HEADER_MAX_SIZE, message = PostConst.HEADER_SIZE)
     private String header;
 
     @NotBlank(message = PostConst.CONTENT_IS_BLANK)
-    @Size(min = 3, message = PostConst.CONTENT_SIZE)
+    @Size(min = 3, max = PostConst.CONTENT_MAX_SIZE, message = PostConst.CONTENT_SIZE)
     private String content;
 
     private Boolean announcement;
